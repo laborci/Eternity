87f690e529ceb4069dc4460e5a00b9d187cecc63
Gergely Laborci <gergely@laborci.hu>
2019-03-11 15:36:46 +0100

    Update AuthenticableRepositoryInterface.php

--- src/Zuul/AuthenticableRepositoryInterface.php
+++ src/Zuul/AuthenticableRepositoryInterface.php
@@ -3,6 +3,6 @@
 interface AuthenticableRepositoryInterface {
 
 	public function authLookup($id):AuthenticableInterface;
-	public function authLoginLookup($login):AuthenticableInterface;
+	public function authLoginLookup($login):?AuthenticableInterface;
 
 }
