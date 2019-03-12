e4e3ec9698a9ec55bfee667e5bd24a28a83cb9c1
Gergely Laborci <gergely@laborci.hu>
2019-03-12 09:54:55 +0100

    launcher

--- src/Eternity/Application/StartupSequence.php
+++ src/Eternity/Application/StartupSequence.php
@@ -1,5 +1,6 @@
 <?php namespace Eternity\Application;
 
+use Eternity\Routing\DomainRouter;
 use Eternity\ServiceManager\Service;
 use Eternity\ServiceManager\SharedService;
 
@@ -31,20 +32,24 @@ class StartupSequence{
 		include env('PROJECT-INI')['SERVICE-REGISTRY'];
 
 		date_default_timezone_set(env('TIMEZONE') ?? env('PROJECT-INI')['TIMEZONE']);
-	}
 
-	public function cli($CLIAPP){
-		if (env('CONTEXT') === 'CLI'){
-			$CLIAPP::Service()->run();
-			die();
-		}
-		return $this;
+		$app = env('APP');
+		$app::Service()->run();
 	}
-
-	public function web($WEBAPP = \Eternity\Application\WebApp::class){
-		/** @var \Eternity\Application\WebApp $webapp */
-		$webapp = $WEBAPP::Service();
-		return $webapp->router();
-	}
+	
+//
+//	public function cli($CLIAPP){
+//		if (env('CONTEXT') === 'CLI'){
+//			$CLIAPP::Service()->run();
+//			die();
+//		}
+//		return $this;
+//	}
+//
+//	public function web($WEBAPP = \Eternity\Application\WebApp::class):DomainRouter{
+//		/** @var \Eternity\Application\WebApp $webapp */
+//		$webapp = $WEBAPP::Service();
+//		return $webapp->router();
+//	}
 
 }
--- src/Eternity/Application/WebApp.php
+++ src/Eternity/Application/WebApp.php
@@ -4,7 +4,7 @@ use Eternity\Logger\ErrorHandlerRegistratorInterface;
 use Eternity\Router\DomainRouter;
 use Eternity\ServiceManager\{Service, SharedService};
 
-class WebApp implements SharedService {
+abstract class WebApp implements SharedService {
 
 	use Service;
 
@@ -15,8 +15,5 @@ class WebApp implements SharedService {
 		$this->domainRouter = $domainRouter;
 	}
 
-	public function router() { return $this->domainRouter; }
-
-	public function run() { }
-
+	abstract public function run();
 }
