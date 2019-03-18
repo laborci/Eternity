a6f38fbaa3dd4b79f4adf36b4c01cec390276b9e
Gergely Laborci <gergely@laborci.hu>
2019-03-18 11:19:42 +0100

    twig deprecated

--- src/Eternity/Factory/TwigFactory.php
+++ src/Eternity/Factory/TwigFactory.php
@@ -9,17 +9,17 @@ class TwigFactory {
 	protected $config;
 	public function __construct(TwigConfigInterface $config) { $this->config = $config; }
 
-	public function factory(): \Twig_Environment {
-		$loader = new \Twig_Loader_Filesystem();
+	public function factory(): \Twig\Environment {
+		$loader = new \Twig\Loader\FilesystemLoader();
 		foreach ($this->config::sources() as $namespace => $path) {
 			$loader->addPath($path, $namespace);
 		}
-		$twig = new \Twig_Environment($loader, [
+		$twig = new \Twig\Environment($loader, [
 			'cache' => $this->config::cache(),
 			'debug' => $this->config::debug(),
 		]);
 		if ($this->config::debug()) {
-			$twig->addExtension(new \Twig_Extension_Debug());
+			$twig->addExtension(new \Twig\Extension\DebugExtension());
 		}
 		return $twig;
 	}
